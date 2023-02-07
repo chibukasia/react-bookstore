00e2a8cde0e65a1b8a9ad0c173753208edf8d1d8
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-07 23:30:03 +0300

    fetched books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,30 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { removeBook } from './bookSlice'
+import { getBooks } from './bookSlice'
+
 function Books() {
 
   const books = useSelector(state=>state)
   const dispatch = useDispatch()
 
   function handleDelete(book){
-    dispatch(removeBook(book))
+    fetch(`http://127.0.0.1:8000/books/${book}/`,{
+        method: "DELETE",
+    })
+    .then(res=>res)
+    .then(()=>dispatch(removeBook(book)))
+    .catch(error=>console.log(error))   
   }
-  
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/books/")
+    .then(res=>{
+        if(res.ok){
+            res.json().then(data=>dispatch(getBooks(data)))
+        }else{
+            res.json().then(console.log)
+        }
+    })
+    .catch(error=>console.log(error))
+  }, [])
+
+  if (books){
   return (
     <div>
         {books.map(book=>{
             return (
-            <>
+            <div key={book.id}>
                 <h2>{book.title}</h2>
-                <h3>{book.author}</h3>
-                <h4>{book.isbn}</h4>
-                <button onClick={()=>handleDelete(book.title)}>Delete</button>
-            </>
+                <h3>{book.isbn}</h3>
+                <h4>{book.pub_date}</h4>
+                <button onClick={()=>handleDelete(book.id)}>Delete</button>
+            </div>
             )
         })}
     </div>
   )
+    }
 }
 
 export default Books
--- src/components/bookSlice.jsx
+++ src/components/bookSlice.jsx
@@ -7,7 +7,6 @@ export function addBook(book){
     }
 }; 
 
-
 export function removeBook(book){
     return {
         type: "books/remove",
@@ -15,13 +14,34 @@ export function removeBook(book){
     }
 };
 
+export function getBooks(books){
+    return {
+        type: "books/get",
+        payload: books
+    }
+}
+
+export function updateBook(book){
+    return {
+        type: "books/update",
+        payload: book,
+    }
+}
+
 export default function bookReducer(state=books, action){
 
     switch(action.type){
         case "books/add":
             return [...state, action.payload]
         case "books/remove":
-            return state.filter(item=>item.title !== action.payload)
+            return state.filter(item=>item.id !== action.payload)
+        case "books/get":
+            return action.payload
+        case "books/update":
+            return state.map(item=>{
+                if (item.id===action.payload.id) return action.payload
+                return item
+            })
         default:
             return state
     }
